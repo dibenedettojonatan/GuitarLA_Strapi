7dd45734339ae175e990d990814fae0d310fdf2f
dibenedetto <dibenedettojonatan@hotmail.com>
2022-07-12 13:03:06 +0200

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c5841a4370e043445825867fe146b489'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '708b5d40-c48e-41a3-9962-15769439e583'
+};
